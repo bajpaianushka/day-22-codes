1f6f50b6d497fffda60663e13a64eff639fe87ed
bajpaianushka <anushka.39934@stu.upes.ac.in>
2026-08-31 21:24:05 +0530

    Add files via upload

--- strong.c
+++ strong.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 int main() {
-	int n, temp, d, f, sum=0;
-	scanf("%d", &n);
-	temp=n;
-	while(temp>0){
-		d=temp%10;
-		f=1;
-		for(int i=1; i<=d; i++)f=f*i;
-		sum=sum+f;
-		temp=temp/10;
+	int n = 123;
+	int temp = n;
+	int d, f, sum = 0;
+
+	while(temp > 0){
+		d = temp % 10;
+		f = 1;
+		for(int i=1; i<=d; i++){
+			f = f * i;
+		}
+		sum = sum + f;
+		temp=temp / 10;
 	}
-	if(sum==n)
+	if(sum == n)
 		printf("Strong number\n");
 	else
 		printf("Not strong number\n");
+
 	return 0;
 }
